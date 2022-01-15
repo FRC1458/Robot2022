6e390974a8542e04585f4314a972702e45580124
ThirtyNinene <thirtyninene@gmail.com>
2022-01-15 14:44:23 -0800

    starting visualization

---
+++ visualization/draw.java
@@ -0,0 +1,15 @@
+'''Need:
+Field draw with scaler
+Main loop with movement input drawn (no rotation used)
+'''
+
+import turtle
+
+def drawField(self):
+	#draw the field here, dont forget the scaler everywhere
+
+def calculatePosition(self, x, y):
+	#given movement calculation information, calculate the new position
+
+def drawBot(self, pos, rot):
+	#given position and rotation, draw it
