a2186ae72c0332e60463e2c7d23efca3d32e6887
NeilGhose <59819042+NeilGhose@users.noreply.github.com>
2022-01-18 16:11:27 -0800

    added swerve code

---
+++ src/main/java/frc/robot/swervedrive/WheelDrive.java
@@ -0,0 +1,9 @@
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import com.revrobotics.CANSparkMax;
+
+public class WheelDrive {
+    private CANSparkMax angleMotor;
+    private CANSparkMax speedMotor;
+    private PIDController pidController;
+
+}
